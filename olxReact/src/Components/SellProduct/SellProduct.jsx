6bf43e39cb9dc8379a4775295fc7cb8be14d4d3c
nikhilrajpk <nikhilrajpk2002@gmail.com>
2025-01-23 12:04:25 +0530

    sell product form validation done

--- olxReact/src/Components/SellProduct/SellProduct.jsx
+++ olxReact/src/Components/SellProduct/SellProduct.jsx
@@ -6,6 +6,9 @@ import useUser from '../../Contexts/UserContext'
 import { useNavigate } from 'react-router-dom'
 
 function SellProduct() {
+  const [isError, setIsError] = useState(false)
+  const [errors, setErrors] = useState('')
+
   const navigate = useNavigate()
 
   const {user} = useUser()
@@ -24,9 +27,31 @@ function SellProduct() {
     product_image : null,
     user_id : user?.id,
   })
+
+  const validateInput = (name, value) => {
+    if (name === 'product_name' && value.trim() === '') return 'Enter product name';
+    if (name === 'price' && value.trim() === '') return 'Enter price';
+    if (
+      name === 'price' &&
+      Number(value) < 0
+    )
+      return 'Enter a valid price';
+    if (name === 'category' && value.trim() === '') return 'Enter category';
+    if (name === 'description' && value.trim() === '') return 'Enter description';
+    return '';
+  };
   
   const handleChange = (e) =>{
     const {name, value} = e.target;
+
+    const errorMessage = validateInput(name, value);
+    if (errorMessage) {
+      setErrors(errorMessage);
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
+
     setProduct({
       ...product, [name] : value
     });
@@ -57,31 +82,23 @@ function SellProduct() {
     if (product.product_image) {
       formData.append('product_image', product.product_image);
     }
-    // if (user?.id) {
-    //   formData.append('user', user.id);
-    // }
-
-    // for (let key in product){
-    //   formData.append(key, product[key])
-    // }
-
-    // Debugging: Log formData entries
-    // console.log("FormData contents:");
-    // for (let [key, value] of formData.entries()) {
-    //   console.log(key, value);
-    // }
+    
     console.log("Auth header:", `Bearer ${localStorage.getItem('access')}`);
     
     try{
       const response = await addProduct(formData)
       console.log('product added successfully', response)
+      setIsError(false)
       navigate('/')
     }catch(error){
-      const errorMessage = error.response?.data?.error || error.response?.data || error.message;
-      console.error('Error adding product:', errorMessage);
-      
-      alert(`Failed to add product: ${errorMessage}`);
+      const errorResponse = error.response?.data || { detail: 'An error occurred' };
+      const errorMessage = Object.values(errorResponse)
+      .flat()
+      .join(', '); 
 
+      console.error('Error during registration:', errorMessage);
+      setErrors(errorMessage); 
+      setIsError(true); 
     }
   };
 
@@ -93,13 +110,18 @@ function SellProduct() {
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWvz4SoS1_MDfmm-HevR3Wgx2er-ZQNaKm_aGkYHdOgkLKJQXj2j0BlV4&s"
                 alt="olx logo" className="sell_img" />
             </div>
+            {
+              isError && <div className='error_box'>{errors}</div>
+            }
             <form onSubmit={handleSubmit} className='sell_form' encType='multipart/form-data'>
                 <input onChange={handleChange} name='product_name' type="text" placeholder='Product Name' className="sell_field" required />
                 <input onChange={handleChange} name='price' type="text" placeholder='Price' className="sell_field" required />
                 <input onChange={handleChange} name='category' type="text" placeholder='Category' className="sell_field" required />
                 <input onChange={handleChange} name='description' type="text" placeholder='Description' className="sell_field" required />
                 <input onChange={handleImageChange} name='product_image' type="file" placeholder='Image' accept='.png, .jpg, .jpeg' className="sell_upload_img" required />
-                <button type='submit' className='sell_button'>Sell</button>
+                {
+                  !isError && <button type='submit' className='sell_button'>Sell</button>
+                }
             </form>
         </div>
     </div>
