f752b672ffe0a05418544fa40e8186d3c3e55764
nikhilrajpk <nikhilrajpk2002@gmail.com>
2025-01-23 13:46:10 +0530

    loader added on the home page

--- olxReact/src/Components/DisplayProduct/DisplayProduct.jsx
+++ olxReact/src/Components/DisplayProduct/DisplayProduct.jsx
@@ -3,14 +3,18 @@ import Card from '../Card/Card'
 import './DisplayProduct.css'
 import { fetchProducts } from '../../services/products'
 import useProduct from '../../Contexts/ProductContext'
+import Loader from '../../utils/Loader/Loader'
 
 function DisplayProduct() {
+  const [loading, setLoading] = useState(false)
+
   // consuming productContext
   const {products, handleProducts, searchTerm} = useProduct()
 
   // fetching products on mount
   useEffect(()=>{
     const getProducts = async () =>{
+      setLoading(true)
       try{
         const data = await fetchProducts();
         console.log(data)
@@ -18,6 +22,8 @@ function DisplayProduct() {
       }catch(error){
         console.error('Error loading products:', error);
         alert('Failed to fetch products. Please log in again.');
+      }finally{
+        setLoading(false)
       }
     };
     getProducts()
@@ -34,10 +40,11 @@ function DisplayProduct() {
     <div className='display_products_div'>
         <h2>Suggested Products</h2>
         <div className='display_products_card_div'>
-          {
-            searchFilterProduct && searchFilterProduct.map((product)=>(
-              < Card prdct={product} key={product.id} />
-            ))
+          { loading ? < Loader /> :(
+              searchFilterProduct && searchFilterProduct.map((product)=>(
+                < Card prdct={product} key={product.id} />
+              ))
+            )
           }
         </div>
     </div>
--- olxReact/src/Components/Profile/Profile.jsx
+++ olxReact/src/Components/Profile/Profile.jsx
@@ -4,6 +4,8 @@ import Arrow from '../Arrow/Arrow'
 import UserProduct from '../UserProduct/UserProduct'
 import useUser from '../../Contexts/UserContext'
 import useProduct from '../../Contexts/ProductContext'
+import no_product_yet from '../../assets/no_product_yet.png'
+
 function Profile() {
   const {user} = useUser()
   const {products} = useProduct()
@@ -27,11 +29,15 @@ function Profile() {
         </div>
         <div className='profile_product_div'>
           {
-            filteredUserProduct.map((product)=>(
-              
-              < UserProduct product={product} key={product.id} />
-              
-            ))
+            filteredUserProduct.length !== 0 ? (
+              filteredUserProduct.map((product)=>(
+                
+                < UserProduct product={product} key={product.id} />
+                
+              ))
+            ) : (
+              <img src={no_product_yet} alt="No products yet" className='no_products_yet' />
+            )
           }
         </div>
     </div>
